43ab8a45ec284dd50f9faf8f89974e315518fa51
ahmad1091 <ahmadtayeb91@gmail.com>
2022-07-12 08:37:02 +0300

    growingPlant

---
+++ codeSignal/growingPlant.js
@@ -0,0 +1,19 @@
+// Caring for a plant can be hard work, but since you tend to it regularly,
+// you have a plant that grows consistently.
+// Each day, its height increases by a fixed amount represented by the integer upSpeed.
+// But due to lack of sunlight, the plant decreases in height every night, by an amount represented by downSpeed.
+
+// Since you grew the plant from a seed, it started at height 0 initially. Given an integer desiredHeight,
+// your task is to find how many days it'll take for the plant to reach this height.
+
+function solution(upSpeed, downSpeed, desiredHeight) {
+  if (desiredHeight <= upSpeed) return 1;
+  let sumSpeed = upSpeed - downSpeed;
+  let count = 0;
+  let reached = 0;
+  while (reached < desiredHeight - downSpeed) {
+    reached += sumSpeed;
+    count++;
+  }
+  return count;
+}
