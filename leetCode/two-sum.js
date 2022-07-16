8ff24c7cb8c058f1ba9529972c88523625b06c07
ahmad1091 <ahmadtayeb91@gmail.com>
2022-07-16 18:05:44 +0300

    update solutionusing hash

--- leetCode/two-sum.js
+++ leetCode/two-sum.js
@@ -18,5 +18,16 @@ var twoSum = function (nums, target) {
   }
   return null;
 };
-
+//////////////////////////solution with hash
+var twoSum = function (nums, target) {
+  let hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    complement = target - nums[i];
+    if (hash[complement] !== undefined) {
+      return [hash[complement], i];
+    }
+    hash[nums[i]] = i;
+  }
+  return [];
+};
 console.log(twoSum([3, 2, -3], 0));
