38e11fb397113a07c49aa7a423f3581045ffa933
ahmad1091 <ahmadtayeb91@gmail.com>
2022-07-20 00:06:36 +0300

    find-pivot-index

--- leetCode/find-pivot-index.js
+++ leetCode/find-pivot-index.js
@@ -2,27 +2,39 @@
  * @param {number[]} nums
  * @return {number}
  */
-var pivotIndex = function (nums) {
-  for (let i = 0; i < nums.length; i++) {
-    nums[i];
-    prefixSum =
-      nums.slice(0, i).length > 0
-        ? nums.slice(0, i).reduce((acc, e) => {
-            return (acc += e);
-          })
-        : 0;
+////////////brute force solution
+// var pivotIndex = function (nums) {
+//   for (let i = 0; i < nums.length; i++) {
+//     nums[i];
+//     prefixSum =
+//       nums.slice(0, i).length > 0
+//         ? nums.slice(0, i).reduce((acc, e) => {
+//             return (acc += e);
+//           })
+//         : 0;
 
-    suffixSum =
-      nums.slice(i + 1).length > 0
-        ? nums.slice(i + 1).reduce((acc, e) => {
-            return (acc += e);
-          })
-        : 0;
-    if (prefixSum == suffixSum) {
-      return i;
-    }
+//     suffixSum =
+//       nums.slice(i + 1).length > 0
+//         ? nums.slice(i + 1).reduce((acc, e) => {
+//             return (acc += e);
+//           })
+//         : 0;
+//     if (prefixSum == suffixSum) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// };
+///////////// O(n) complexity
+var pivotIndex = function (nums) {
+  let totalSum = nums.reduce((acc, e) => (acc += e));
+  let prefixSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (prefixSum == totalSum - nums[i]) return i;
+    prefixSum += nums[i];
+    totalSum -= nums[i];
   }
   return -1;
 };
 
-console.log(pivotIndex([2, 1, -1]));
+console.log(pivotIndex([1, 2, 3]));
