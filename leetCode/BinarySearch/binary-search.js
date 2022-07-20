7efd5fb284007183c4caf67d6158aa61b7a588da
ahmad1091 <ahmadtayeb91@gmail.com>
2022-07-20 19:50:54 +0300

    Recursive Solution

--- leetCode/BinarySearch/binary-search.js
+++ leetCode/BinarySearch/binary-search.js
@@ -1,3 +1,4 @@
+/////// First Solution
 var search = function (nums, target) {
   let start = 0;
   let end = nums.length;
@@ -12,5 +13,17 @@ var search = function (nums, target) {
   }
   return -1;
 };
+/////// Recursive Solution
+var search = function (nums, target,start = 0 , end = nums.length) {
+  let middle = Math.floor((end + start) / 2);
+  if (nums[middle] == target) return middle;
+if (start >= end) {
+  return -1
+}
+return target < nums[middle] ?
+        search(nums,target,start,middle):
+        search(nums,target,middle+1,end);
 
-console.log(search([1,2,3,4,5],4))
+}
+
+console.log(search([1,2,3,4,5],3))
