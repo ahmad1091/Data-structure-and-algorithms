4f3449a166599423e0c42974e6dd4f3aba203ba0
ahmad1091 <ahmadtayeb91@gmail.com>
2022-07-25 20:31:47 +0300

    Selection sort

--- leetCode/sort-an-array.js
+++ leetCode/sort-an-array.js
@@ -19,9 +19,27 @@ var sortArray = function (nums) {
         if (nums[j]>nums[j+1]) {
             [nums[j+1],nums[j]]=[nums[j],nums[j+1]];
         }
-        
-       } 
+
+       }
     }
     return nums;
 }
+
+// "Selection Sort" The space complexity for the algorithm is O(1)
+// and the average time complexity is O(n²).
+var sortArray = function (nums) {
+  let min;
+  for (let i = 0; i < nums.length; i++) {
+    min = i;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] < nums[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      [nums[i], nums[min]] = [nums[min], nums[i]];
+    }
+  }
+  return nums;
+};
 console.log(sortArray([5, 2, 3, 1]));
