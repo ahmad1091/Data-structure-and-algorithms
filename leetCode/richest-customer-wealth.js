29206dc9ddaa2b971e58a113ccfed82a9633303f
ahmad1091 <ahmadtayeb91@gmail.com>
2022-07-27 15:04:33 +0300

    richest-customer-wealth

--- leetCode/richest-customer-wealth.js
+++ leetCode/richest-customer-wealth.js
@@ -12,3 +12,24 @@
     }
     return max;
 };
+
+// optimize the code
+
+var maximumWealth = function (accounts) {
+    let max = -Infinity;
+    for (let idx = 0; idx < accounts.length; idx++) {
+      var i = 0;
+      var j = accounts[idx].length - 1;
+      var sum = 0;
+      while (i <= j) {
+        sum =
+          sum + (i == j ? accounts[idx][i] : accounts[idx][i] + accounts[idx][j]);
+        i++;
+        j--;
+      }
+      if (sum > max) {
+        max = sum;
+      }
+    }
+    return max;
+  };
