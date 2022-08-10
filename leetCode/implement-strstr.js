517234e4dd799c530f9e9511d9f13ac47b564ff7
ahmad1091 <ahmadtayeb91@gmail.com>
2022-08-10 11:53:04 +0300

    implement-strstr

---
+++ leetCode/implement-strstr.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+// brut force solution
+var strStr = function (haystack, needle) {
+  for (let i = 0; i < haystack.length; i++) {
+    let match = true;
+    for (let j = 0; j < needle.length; j++) {
+      if (haystack[i + j] !== needle[j]) {
+        match = false;
+        break;
+      }
+    }
+    if (match) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+console.log(strStr("mississippi", "issip"));
