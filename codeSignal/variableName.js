a07ef6c7e56ee529e16749e6ff8543a25de03a12
ahmad1091 <ahmadtayeb91@gmail.com>
2022-08-12 19:56:29 +0300

    variableName solution

--- codeSignal/variableName.js
+++ codeSignal/variableName.js
@@ -10,3 +10,11 @@
 // solution(name) = false;
 // For name = "2w2", the output should be
 // solution(name) = false.
+
+function solution(name) {
+  let pattern =
+    /\b\d|\-|\[|\]|\{|\}| |\(|\)|\*|\?|\,|\'|\"|\`|\^|\+|\=|\&|\#|\@|\~|\$|\%|\<|\>|\:|\;|\||\!|\/|\./g;
+  let result = name.search(pattern);
+  console.log("result", result, result <= 0);
+  return result < 0;
+}
