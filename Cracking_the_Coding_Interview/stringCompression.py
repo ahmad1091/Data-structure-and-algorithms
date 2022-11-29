02860f882055d9d5874a65669ee2a752ed5fd438
ahmad1091 <ahmadtayeb91@gmail.com>
2022-11-29 09:51:22 +0200

    better solution for stringCompression

--- Cracking_the_Coding_Interview/stringCompression.py
+++ Cracking_the_Coding_Interview/stringCompression.py
@@ -4,7 +4,8 @@
 # the original string. You can assume the string has only uppercase and lowercase letters (a - z).
 
 #  BAD solution:
-
+#  The runtime is O(p + k 2), where p is the size of the original string and k is the number of character sequences.
+#  It's slow because string concatenation operates in O( n2) time
 def compress(s):
     compressedStr = ''
     charCount = 0
@@ -16,5 +17,20 @@ def compress(s):
             charCount = 0
     return s if len(compressedStr) > len(s) else compressedStr
 
+
+
+# Better solution uses list
+def compress(s):
+    compressedArr = []
+    charCount = 0
+
+    for i in range(len(s)):
+        charCount += 1
+        if(i+1 >= len(s) or s[i] != s[i+1]):
+            compressedArr.append(s[i])
+            compressedArr.append(str(charCount))
+            charCount = 0
+
+    return s if len(compressedArr) > len(s) else ''.join(compressedArr)
 
 print(compress('aabcccccaaa'))
