68b2335fc34863a8b199f0d2449ccbcffc4ea229
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-03 11:24:05 +0200

    Cracking_the_Coding_Interview/chapter2/removeDups

--- Cracking_the_Coding_Interview/chapter2/removeDups.py
+++ Cracking_the_Coding_Interview/chapter2/removeDups.py
@@ -6,23 +6,42 @@ class ListNode:
         self.val = val
         self.next = next
 
+# solution 1:
+# class Solution:
+#     def deleteDuplicates(self, head):
+#         slow = head
+
+#         while(slow):
+#             fast = slow
+#             while (fast.next):
+#                 if(fast.next.val == slow.val):
+#                     fast.next = fast.next.next
+#                 else:
+#                     fast = fast.next
+            
+#             slow = slow.next
+
+#         for i in range(6): # this loop is for testing
+#             print(head.val)
+#             head = head.next
+
+# solution 2:
 class Solution:
     def deleteDuplicates(self, head):
-        slow = head
-
-        while(slow):
-            fast = slow
-            while (fast.next):
-                if(fast.next.val == slow.val):
-                    fast.next = fast.next.next
-                else:
-                    fast = fast.next
-            
-            slow = slow.next
+        hashSet = {}
+        prev = ListNode(None)
+        while(head != None):
+            if head.val in hashSet:
+                prev.next = head.next
+            else:
+                hashSet[head.val] = 1
+                prev = head
+                print(head.val)
+            head = head.next
 
         for i in range(6): # this loop is for testing
-            print(head.val)
-            head = head.next
+            print(prev.val)
+            prev = prev.next
 
 
 myNode = ListNode(1 ,ListNode(2, ListNode(2, ListNode(3, ListNode(5, ListNode(3, ListNode(4, ListNode(6))))))))
