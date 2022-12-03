9f260ff4ac53b698dd2ef10c7dbaac6a8560e3f4
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-03 09:32:47 +0200

    leetCode/LinkedList/remove-duplicates-from-sorted-list

--- leetCode/LinkedList/remove-duplicates-from-sorted-list.py
+++ leetCode/LinkedList/remove-duplicates-from-sorted-list.py
@@ -1,3 +1,7 @@
+# 83. Remove Duplicates from Sorted List
+
+# Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
