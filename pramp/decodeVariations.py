63a79bb02fc6a3dbda41903e0f3b4cba78160b20
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-04 17:52:28 +0200

    pramp/decodeVariations

---
+++ pramp/decodeVariations.py
@@ -0,0 +1,29 @@
+# Decode Variations
+# A letter can be encoded to a number in the following way:
+
+# 'A' -> '1', 'B' -> '2', 'C' -> '3', ..., 'Z' -> '26'
+# A message is a string of uppercase letters, and it is encoded first using this scheme. For example, 'AZB' -> '1262'
+
+# Given a string of digits S from 0-9 representing an encoded message, return the number of ways to decode it.
+
+def decodeVariations(S):
+	"""
+	@param S: str
+	@return: int
+	"""
+    dp = { len(s) : 1 }
+        
+    def dfs(i):
+        if i in dp:
+            return dp[i]
+        if s[i] == '0' :
+            return 0
+        res = dfs(i+1)
+            
+        if(i+1 < len(s) and (s[i] =='1' or s[i] == '2' and s[i+1] in '0123456')):
+            res += dfs(i+2)
+                
+        dp[i] = res 
+        return res 
+        
+    return dfs(0)
