94507bebb82aeabb7f358575bee150bed96e53da
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-06 20:19:21 +0200

    Cracking_the_Coding_Interview/chapter2/sumList sol 2

--- Cracking_the_Coding_Interview/chapter2/sumList.py
+++ Cracking_the_Coding_Interview/chapter2/sumList.py
@@ -7,6 +7,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Solution 1: Recursion
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         def addLists(l1, l2, carry):
@@ -32,3 +34,42 @@ class Solution:
                 result.next = more
             return result
         return addLists(l1, l2,0)
+# Solution 2:
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        resList = ListNode()
+        dummy = resList
+        carrier = 0
+        temp1 = l1
+        temp2 = l2
+        count1 = 0
+        count2 = 0
+
+        while(temp1):
+            count1 += 1
+            temp1 = temp1.next
+        
+        while(temp2):
+            count2 += 1
+            temp2 = temp2.next
+
+        longer = l1 if count1 >= count2 else l2
+        shorter = l1 if count1< count2 else l2
+ 
+        while(longer):
+            if(shorter):
+                sumOf2 = longer.val + shorter.val + carrier
+                shorter = shorter.next
+            else:
+                sumOf2 = longer.val + carrier
+            carrier = 0
+            if sumOf2 > 9:
+                sumOf2 = sumOf2 % 10
+                carrier = 1
+            dummy.next = ListNode(sumOf2)
+            dummy = dummy.next
+            longer = longer.next
+
+        if carrier>0:
+            dummy.next = ListNode(carrier)
+        return resList.next
