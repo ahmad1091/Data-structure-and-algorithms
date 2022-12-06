7a1ff4a9350553807caeb36a9e4c746731695fcd
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-06 22:04:56 +0200

    Cracking_the_Coding_Interview/chapter2/palindrome

--- Cracking_the_Coding_Interview/chapter2/palindrome.py
+++ Cracking_the_Coding_Interview/chapter2/palindrome.py
@@ -5,6 +5,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# Solution 1:
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         testList = []
@@ -15,3 +16,27 @@ class Solution:
         string = ''.join(str(x) for x in testList)
         testList.reverse()
         return string == ''.join(str(x) for x in testList)
+
+# Solution 2:
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        slow = head 
+        fast = head
+
+        arr = [] 
+        while(fast != None and fast.next != None):
+            arr.append(slow.val)
+            slow = slow.next
+            fast = fast.next.next
+
+        if (fast != None):
+            slow = slow.next
+
+        while slow:
+            top = arr.pop()
+
+            if(top != slow.val):
+                return False
+            slow = slow.next
+        
+        return True
