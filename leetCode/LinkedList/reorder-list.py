6e033ccec33ea764bbbe0d3f35d6330130207266
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-09 18:04:59 +0200

    leetCode/LinkedList/reorder-list

---
+++ leetCode/LinkedList/reorder-list.py
@@ -0,0 +1,50 @@
+# 143. Reorder List
+
+# You are given the head of a singly linked-list. The list can be represented as:
+
+# L0 → L1 → … → Ln - 1 → Ln
+# Reorder the list to be on the following form:
+
+# L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+# You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        rev = None
+        curr = head
+        count = 0
+        while(curr):
+            count +=1
+            node = rev
+            rev = ListNode(curr.val,node)
+            curr = curr.next
+        result = ListNode()
+
+        toggle =True
+        while(count/2 > 0):
+            if(toggle):
+                nxt2 = head.next
+                head.next = None
+                result.next = head
+                head = nxt2
+                toggle = not toggle
+            else:
+                nxt3 = rev.next
+                rev.next = None
+                result.next = rev
+                rev = nxt3
+                toggle = not toggle
+            count -=1
+
+            result = result.next
+            
+        return result
