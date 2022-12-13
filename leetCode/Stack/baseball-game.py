baad9f6cba4533e86a5e9221795c5869ae11229b
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-13 14:58:42 +0200

    leetCode/Stack/baseball-game

--- leetCode/Stack/baseball-game.py
+++ leetCode/Stack/baseball-game.py
@@ -56,3 +56,20 @@ class Solution:
                 arr.append(val)
     
         return sum(arr)
+
+# sol 3 The optimal sol 
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+
+        for op in operations:
+            if op == '+':
+                stack.append(stack[-1] + stack[-2])
+            elif op == 'D':
+                stack.append(2 * stack[-1])
+            elif op == 'C':
+                stack.pop()
+            else:
+                stack.append(int(op))
+    
+        return sum(stack)
