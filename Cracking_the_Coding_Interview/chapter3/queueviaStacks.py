574ac4a7a1b2c52551bc636c691c147001e34b13
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-15 01:11:26 +0200

    Cracking_the_Coding_Interview/chapter3/queueviaStacks

---
+++ Cracking_the_Coding_Interview/chapter3/queueviaStacks.py
@@ -0,0 +1,26 @@
+# Queue via Stacks: Implement a MyQueue class which implements a queue using two stacks.
+
+class MyQueue:
+
+    def __init__(self):
+        self.stackNewest = []
+        self.stackOldest = []
+
+    def size(self):
+        return len(self.stackNewest) + len(self.stackOldest)
+
+    def add(self,val):
+        self.stackNewest.append(val)
+
+    def add(self):
+        if len(self.stackOldest) == 0:
+            while(self.stackNewest):
+                self.stackOldest.append(stackNewest.pop())
+
+    def peek(self):
+        self.shiftStacks()
+        return self.stackOldest[-1]
+
+    def remove(self):
+        self.shiftStacks()
+        return self.stackOldest.pop()
