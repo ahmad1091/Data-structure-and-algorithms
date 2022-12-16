2405b0dddb905ea3531b98c5cb6e385ff8c78c1b
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-16 18:01:12 +0200

    leetCode/Heap(PriorityQueue)/top-k-frequent-elements using bucket sort

--- leetCode/Heap(PriorityQueue)/top-k-frequent-elements.py
+++ leetCode/Heap(PriorityQueue)/top-k-frequent-elements.py
@@ -13,7 +13,7 @@ class Solution:
         return a[-k:]
 
 
-# solution 1: using max_heap
+# solution 2: using max_heap
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
@@ -29,3 +29,22 @@ class Solution:
             k -= 1
         
         return result
+
+# solution 3: using Bucket Sort
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dict = {}
+        freq = [[] for i in range(len(nums)+1)]
+
+        for item in nums:
+            dict[item] = dict.get(item, 0) + 1
+
+        for n, c in dict.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
