17e5e16b77cbd81cb9e9a48e750a8fa979afb64d
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-16 16:51:39 +0200

    leetCode/Heap(PriorityQueue)/kth-largest-element-in-an-array

--- leetCode/Heap(PriorityQueue)/kth-largest-element-in-an-array.py
+++ leetCode/Heap(PriorityQueue)/kth-largest-element-in-an-array.py
@@ -6,7 +6,30 @@
 
 # You must solve it in O(n) time complexity.
 
+# sol 1: using built-in sort  O(nlogn)
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         nums.sort()
         return nums[-k]
+
+
+# sol 2: using quickSelect
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        k = len(nums) - k
+
+        def quickSelect(l,r):
+            p = l
+            for i in range(l,r):
+                if nums[r] >= nums[i]:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+            nums[p], nums[r] = nums[r], nums[p]
+
+            if p > k:   return quickSelect(l, p - 1) 
+            elif p < k: return quickSelect(p + 1, r) 
+            else:       return nums[p]
+
+        return quickSelect(0, len(nums) - 1)
