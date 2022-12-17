e863798c0383694d168eaa01708d4115e38be6be
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-17 21:01:34 +0200

    leetCode/trapping-rain-water

--- leetCode/trapping-rain-water.py
+++ leetCode/trapping-rain-water.py
@@ -2,6 +2,7 @@
 
 # Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 
+# sol 1: Time: O(n) Space: O(n)
 class Solution:
     def trap(self, height: List[int]) -> int:
         maxLeft = [None] * len(height)
@@ -22,5 +23,27 @@ class Solution:
             val = min(maxRight[i],maxLeft[i]) - height[i]
             if val >= 0:
                 qty += val
+
+        return qty
+
+# Sol 2: Time: O(n) Space: O(1) Two Pointers
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:return 0
+
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        qty = 0
+        
+        while(l < r):
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                qty += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                qty += rightMax - height[r]
 
         return qty
