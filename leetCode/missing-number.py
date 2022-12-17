507bb61c1b0d83a465229287294cc862237ffef7
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-18 01:29:00 +0200

    leetCode/Missing Number sol 2

--- leetCode/missing-number.py
+++ leetCode/missing-number.py
@@ -2,7 +2,7 @@
 
 # Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
-
+# sol 1:
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         nums.sort()
@@ -10,3 +10,12 @@ class Solution:
         for i in range(len(nums) + 1):
             if (i not in nums  or i != nums[i]):
                 return i
+
+# sol 2:
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = len(nums)
+
+        for i in range(len(nums)):
+            res += i - nums[i]
+        return res
