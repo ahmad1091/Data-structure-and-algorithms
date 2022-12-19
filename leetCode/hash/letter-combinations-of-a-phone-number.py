31a41d90674882fcfe4ee19f3732a7e3257353d3
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-20 00:26:24 +0200

    leetCode/hash/letter-combinations-of-a-phone-number

---
+++ leetCode/hash/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,28 @@
+# 17. Letter Combinations of a Phone Number
+
+# Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+# A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        res = ['']
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        for i in digits:
+            s = digitToChar[i]
+            res = [a + b for a in res for b in s]
+        
+        return res
+ 
