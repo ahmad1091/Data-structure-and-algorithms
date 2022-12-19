bd1312d498054082fe60acd6a78d15e81956c1ef
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-20 00:28:09 +0200

    leetCode/hash/letter-combinations-of-a-phone-number sol 2

--- leetCode/hash/letter-combinations-of-a-phone-number.py
+++ leetCode/hash/letter-combinations-of-a-phone-number.py
@@ -4,6 +4,7 @@
 
 # A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 
+# Sol 1: brute Force
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
@@ -26,3 +27,29 @@ class Solution:
         
         return res
  
+ # Sol 1: Back track
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            for c in digitToChar[digits[i]]:
+                backtrack(i + 1, curStr + c)
+
+        if digits:
+            backtrack(0, "")
+
+        return res
