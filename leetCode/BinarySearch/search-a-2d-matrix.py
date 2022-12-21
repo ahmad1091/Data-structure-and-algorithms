25a8fd68927004ad1e18974541062803155d42a1
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-22 01:37:12 +0200

    leetCode/BinarySearch/search-a-2d-matrix.py

---
+++ leetCode/BinarySearch/search-a-2d-matrix.py
@@ -0,0 +1,20 @@
+# 74. Search a 2D Matrix
+
+# Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+# Integers in each row are sorted from left to right.
+# The first integer of each row is greater than the last integer of the previous row.
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        r, c = 0, len(matrix[0]) - 1
+
+        while r < len(matrix) and c >=0:
+            if matrix[r][c] == target:
+                return True
+            elif matrix[r][c] > target:
+                c -= 1
+            else:
+                r += 1
+
+        return False
