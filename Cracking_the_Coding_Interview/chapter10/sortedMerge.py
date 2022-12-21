0ca25f7a7d2a698acc9c7d76674b9bfdd3ea6f23
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-21 15:53:46 +0200

    without allocate additional space

--- Cracking_the_Coding_Interview/chapter10/sortedMerge.py
+++ Cracking_the_Coding_Interview/chapter10/sortedMerge.py
@@ -1,6 +1,7 @@
 # Sorted Merge: You are given two sorted arrays, A and B, where A has a large enough buffer at the
 # end to hold B. Write a method to merge B into A in sorted order.
 
+# sol 1 :
 def merge(a,b):
     res = []
     
@@ -19,3 +20,26 @@ def merge(a,b):
     return res
 
 print(merge([4,5,6],[0,1,1,2]))
+
+# sol 2: without allocate additional space
+def merge(a,b):
+    aIdx = len(a) - 1
+    bIdx = len(b) - 1
+    mergeIdx = len(a) + len(b) - 1
+
+    while bIdx >= 0:
+        if aIdx >= 0 and a[aIdx] > b[bIdx]:
+            a[mergeIdx] = a[aIdx]
+            aIdx -= 1
+        else:
+            a[mergeIdx] = b[bIdx]
+            bIdx -= 1
+
+        mergeIdx -= 1
+
+a = array.array([2,2,4,5,6])
+b = [0,1,1,2]
+
+merge(a,b)
+print(a)
+
