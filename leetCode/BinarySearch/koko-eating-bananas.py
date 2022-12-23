8333fa0d48722cee6213cface851c4fd4f1a3a76
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-23 20:15:42 +0200

    leetCode/BinarySearch/koko-eating-bananas

--- leetCode/BinarySearch/koko-eating-bananas.py
+++ leetCode/BinarySearch/koko-eating-bananas.py
@@ -8,3 +8,27 @@
 
 # Return the minimum integer k such that she can eat all the bananas within h hours.
 
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        mx = max(piles)
+        minH = 10000000
+        for i in range(1,mx+1):
+            hTrack = h
+            c = 0 
+            for j in range(len(piles)):
+                if hTrack <= 0:
+                    break 
+                print(piles[j],i,ceil(piles[j]/i))
+                if piles[j] > i:
+                    hTrack -= ceil(piles[j]/i)
+                    if hTrack < 0:
+                        break 
+                else:
+                    hTrack -= 1
+                c += 1
+
+            if i < minH and c == len(piles):
+                minH = i
+
+
+        return minH
