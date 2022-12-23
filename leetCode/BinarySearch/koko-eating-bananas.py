b6022b090ae7c4b55a675beb4a2f0b09d0481531
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-23 20:58:25 +0200

    leetCode/BinarySearch/koko-eating-bananas

--- leetCode/BinarySearch/koko-eating-bananas.py
+++ leetCode/BinarySearch/koko-eating-bananas.py
@@ -32,3 +32,24 @@ class Solution:
 
 
         return minH
+
+# sol 2: BST
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res =  r
+
+        while r >= l:
+            k = (r + l) // 2
+            hours = 0
+
+            for p in piles:
+                hours += ceil(p / k)
+
+            if hours <= h:
+                res = min(res,k)
+                r = k - 1
+            else:
+                l = k + 1
+
+        return res
