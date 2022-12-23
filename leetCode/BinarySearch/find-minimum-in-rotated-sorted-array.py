07e5c7ac5a494a6cc10b873954519272611adf99
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-24 01:05:16 +0200

    leetCode/BinarySearch/find-minimum-in-rotated-sorted-array.py

--- leetCode/BinarySearch/find-minimum-in-rotated-sorted-array.py
+++ leetCode/BinarySearch/find-minimum-in-rotated-sorted-array.py
@@ -18,3 +18,25 @@ class Solution:
                 return nums[i]
 
         return nums[0]
+
+# sol 2: BST
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        res = nums[0]
+
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
+
+            mid = (r + l) // 2
+            res = min(res, nums[mid])
+            if nums[l] <= nums[mid]:
+                 l = mid + 1
+            else:
+                r = mid - 1
+
+        return res
