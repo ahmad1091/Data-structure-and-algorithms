ca724185d95bc925faa130f26e9ae591425080d2
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-27 21:27:58 +0200

    Cracking_the_Coding_Interview/chapter8

---
+++ Cracking_the_Coding_Interview/chapter8/booleanEvaluation.py
@@ -0,0 +1,3 @@
+# Boolean Evaluation: Given a boolean expression consisting of the symbols e (false), 1 (true), &
+# (AND), I (OR), and" (XOR), and a desired boolean result value result, implement a function to
+# count the number of ways of parenthesizing the expression such that it evaluates to result .
---
+++ Cracking_the_Coding_Interview/chapter8/permutationsWithDups.py
@@ -0,0 +1,3 @@
+# Permutations with Dups: Write a method to compute all permutations of a string whose charac-
+# ters are not necessarily unique. The list of permutations should not have duplicates.
+
