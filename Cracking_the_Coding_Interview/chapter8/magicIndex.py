f1df487a90472ec900c891bee68df436269a1099
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-29 00:45:46 +0200

    Cracking_the_Coding_Interview/chapter8/magicIndex.py

--- Cracking_the_Coding_Interview/chapter8/magicIndex.py
+++ Cracking_the_Coding_Interview/chapter8/magicIndex.py
@@ -1,3 +1,12 @@
 # Magic Index: A magic index in an array A [e ... n -1] is defined to be an index such that A[ i] =
 # i. Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in
 # array A.
+
+# Sol 1:
+def magicSlow(arr: list[int]) -> int:
+    for i in range(len(arr):
+        if arr[i] == i:
+            return i
+    
+    return -1
+
