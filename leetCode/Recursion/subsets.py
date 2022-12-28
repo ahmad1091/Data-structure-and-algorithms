31b85825a9d603273a9b5005d7095057324406a0
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-29 01:49:28 +0200

    leetCode/Recursion/subsets.py

--- leetCode/Recursion/subsets.py
+++ leetCode/Recursion/subsets.py
@@ -6,6 +6,7 @@
 
 # The solution set must not contain duplicate subsets. Return the solution in any order.
 
+# Solution #1: Recursion
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         def subsets(nums: List[int], i: int) -> List[List[int]]:
@@ -30,4 +31,24 @@ class Solution:
 
         return subsets(nums, 0)
 
+# Solution #2: Combinatorics
+class Solution:
+    def convertItToSet(self, x: int, arr: list[int]):
+        subset = []
+        idx = 0
+
+        for k in range(x - 1, 0, -1):
+            if (k & 1) == 1:
+                subset.append(arr[idx]) 
+            idx += 1
             
+        return subset
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        allsubsets = []
+        max = 1 << len(nums)
+
+        for k in range(max):
+            subset = self.convertItToSet(k, nums)
+            allsubsets.append(subset)
+
+        return allsubsets
