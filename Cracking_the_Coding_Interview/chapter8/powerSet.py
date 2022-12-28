a6f0af95712601a3d9bad69ea16527f271801539
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-29 01:24:27 +0200

    Cracking_the_Coding_Interview/chapter8/powerSet.py

--- Cracking_the_Coding_Interview/chapter8/powerSet.py
+++ Cracking_the_Coding_Interview/chapter8/powerSet.py
@@ -1,2 +1,25 @@
 # Power Set: Write a method to return all subsets of a set.
 
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def subsets(nums: List[int], i: int) -> List[List[int]]:
+            allsubsets = []
+
+            if len(nums) == i:
+                allsubsets.append([]) 
+            else:
+                allsubsets = subsets(nums, i + 1)
+                item = nums[i]
+                moresubsets = []
+
+                for subset in allsubsets:
+                    newsubset = []
+                    newsubset += subset
+                    newsubset.append(item)
+                    moresubsets.append(newsubset)
+
+                allsubsets += moresubsets
+
+            return allsubsets
+
+        return subsets(nums, 0)
