164a38d0c0ae630a2c4e1560c83bf2b8d4cb1407
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-28 14:12:51 +0200

    Cracking_the_Coding_Interview/chapter8/tripleStep.py

--- Cracking_the_Coding_Interview/chapter8/tripleStep.py
+++ Cracking_the_Coding_Interview/chapter8/tripleStep.py
@@ -3,12 +3,25 @@
 # stairs.
 
 # SOL 1: runtime of this algorithm is exponential (roughly O( 3 n )
-
 def countWays(n):
     if n < 0:
         return 0
-    if n == 0:
+    elif n == 0:
         return 1
     else:
         return countWays(n - 1) + countWays(n - 2) + countWays(n - 3)
+
+# SOL 2: Memoization Solution
+def countWays(n):
+    memo = [-1] * n + 1
+    def countWays(i, m:list[int]):
+        if i < 0:
+            return 0
+        elif i == 0:
+            return 1
+        else:
+            memo[i] = countWays(n - 1) + countWays(n - 2) + countWays(n - 3)
+            return memo[i]
+
+
         
