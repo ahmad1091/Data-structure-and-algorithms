f4b590159268de1c5740593dccac96ba584898ac
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-29 02:09:53 +0200

    Cracking_the_Coding_Interview/chapter8/powerSet.py

--- Cracking_the_Coding_Interview/chapter8/powerSet.py
+++ Cracking_the_Coding_Interview/chapter8/powerSet.py
@@ -23,3 +23,24 @@ class Solution:
             return allsubsets
 
         return subsets(nums, 0)
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        allsubsets = []
+        subset = []
+        def dfs(i: int) -> List[List[int]]:
+            if len(nums) <= i:
+                allsubsets.append(subset.copy()) 
+                return
+
+            subset.append(nums[i])
+            dfs(i + 1)
+
+            subset.pop()
+            dfs(i + 1)
+        
+        dfs(0)
+        return allsubsets
+
+            
