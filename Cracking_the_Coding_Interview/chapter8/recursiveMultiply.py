859e21b0a080a32c25af3a958d7e31ccdee25f3e
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-29 14:09:03 +0200

    Cracking_the_Coding_Interview/chapter8/recursiveMultiply.py

--- Cracking_the_Coding_Interview/chapter8/recursiveMultiply.py
+++ Cracking_the_Coding_Interview/chapter8/recursiveMultiply.py
@@ -1,3 +1,10 @@
 # Recursive Multiply: Write a recursive function to multiply two positive integers without using the
 # * operator. You can use addition, subtraction, and bit shifting, but you should minimize the number
 # of those operations.
+
+def multiply(A: int, B: inr) -> int {
+    if A == 0 or B == 0:
+        return 0
+
+    max, min = max(A, B), min(A, B)
+    return max + multiply(max, min - 1);
