1efd70b18ebb21a38f5fbc48cf4d46e964bfa003
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-31 00:26:06 +0200

    leetCode/Recursion/permutations-ii.py

--- leetCode/Recursion/permutations-ii.py
+++ leetCode/Recursion/permutations-ii.py
@@ -22,3 +22,31 @@ class Solution:
 
         arr = set(tuple(x) for x in result)
         return list(list(x) for x in arr)
+
+# sol 2:
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        perm = []
+        count = {n:0 for n in nums}
+
+        for n in nums:
+            count[n] +=1
+
+        def dfs():
+            if len(nums) == len(perm):
+                result.append(perm[:])
+                return
+
+            for n in count:
+                if count[n] > 0:
+                    perm.append(n)
+                    count[n] -= 1
+
+                    dfs()
+
+                    count[n] += 1
+                    perm.pop()
+                    
+        dfs()  
+        return result
