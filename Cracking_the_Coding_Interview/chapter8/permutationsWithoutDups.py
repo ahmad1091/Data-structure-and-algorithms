7b2b4eb718ed5cfd6eadc70209679ec1ae231201
ahmad1091 <ahmadtayeb91@gmail.com>
2022-12-30 16:34:28 +0200

    Cracking_the_Coding_Interview/chapter8/permutationsWithoutDups.py

--- Cracking_the_Coding_Interview/chapter8/permutationsWithoutDups.py
+++ Cracking_the_Coding_Interview/chapter8/permutationsWithoutDups.py
@@ -1,2 +1,23 @@
 # Permutations without Dups: Write a method to compute all permutations of a string of unique
 # characters.
+
+class Solution:
+    def permutation(self, S: str) -> List[str]:
+        def dfs(u, t):
+            if u == n:
+                ans.append(''.join(t))
+                return
+            for i in range(n):
+                if vis[i]:
+                    continue
+                vis[i] = True
+                t.append(S[i])
+                dfs(u + 1, t)
+                t.pop()
+                vis[i] = False
+
+        n = len(S)
+        vis = [False] * n
+        ans = []
+        dfs(0, [])
+        return ans
