638d78511dbcb1f183bf60d401eb2890b04989ed
ahmad1091 <ahmadtayeb91@gmail.com>
2023-01-01 00:05:24 +0200

    leetCode/TwoPointers/valid-palindrome.py 02 pointers solution

--- leetCode/TwoPointers/valid-palindrome.py
+++ leetCode/TwoPointers/valid-palindrome.py
@@ -3,7 +3,7 @@
 # A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 
 # Given a string s, return true if it is a palindrome, or false otherwise.
-
+# sol 1: trivial
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         modified = ''
@@ -12,6 +12,7 @@ class Solution:
                 modified += l.lower()
         return modified == modified[::-1]
 
+# sol 2: recursion
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         def toChar(s: str) -> str:
@@ -28,3 +29,19 @@ class Solution:
                 return s[0] == s[-1] and isPal(s[1:-1])
 
         return isPal(toChar(s))
+
+# sol 3: towPointer
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        mod = ''
+        for c in s.lower():
+            if c.isalpha() or c.isnumeric():
+               mod += c
+               
+        l, r = 0, len(mod) - 1
+        while l < r:
+            if mod[l] != mod[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
