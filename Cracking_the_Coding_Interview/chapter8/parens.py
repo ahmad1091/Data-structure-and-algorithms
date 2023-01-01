47e908c1d9e6b4e9d093cb03fe0fce2fe85ddca3
ahmad1091 <ahmadtayeb91@gmail.com>
2023-01-01 22:50:06 +0200

    Cracking_the_Coding_Interview/chapter8/parens.py

--- Cracking_the_Coding_Interview/chapter8/parens.py
+++ Cracking_the_Coding_Interview/chapter8/parens.py
@@ -1,2 +1,25 @@
 # Parens: Implement an algorithm to print all valid (e.g., properly opened and closed) combinations
 # of n pairs of parentheses.
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        res = [] 
+
+        def backTrack(openN, closeN):
+            if openN == closeN == n:
+                res.append(''.join(stack))
+                return
+
+            if openN < n:
+                stack.append('(')
+                backTrack(openN + 1, closeN)
+                stack.pop()
+
+            if closeN < openN:
+                stack.append(')')
+                backTrack(openN, closeN + 1)
+                stack.pop()
+
+        backTrack(0, 0)
+        return res
