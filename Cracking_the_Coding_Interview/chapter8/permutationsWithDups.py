dde03e1f8bde825ec48c3836b16cd9dbff7fe58d
ahmad1091 <ahmadtayeb91@gmail.com>
2023-01-01 22:11:13 +0200

    Cracking_the_Coding_Interview/chapter8/permutationsWithDups.py

--- Cracking_the_Coding_Interview/chapter8/permutationsWithDups.py
+++ Cracking_the_Coding_Interview/chapter8/permutationsWithDups.py
@@ -1,3 +1,21 @@
 # Permutations with Dups: Write a method to compute all permutations of a string whose charac-
 # ters are not necessarily unique. The list of permutations should not have duplicates.
 
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        if len(nums) == 1:
+            return [nums[:]]
+
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+
+            for p in perms:
+                p.append(n)
+            
+            result.extend(perms)
+            nums.append(n)
+
+        return result
