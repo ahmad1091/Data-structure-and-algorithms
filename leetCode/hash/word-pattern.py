6101ceacb8ada0d50605223ce23b22be6d179d64
ahmad1091 <ahmadtayeb91@gmail.com>
2023-01-02 17:46:06 +0200

    cleaner solution

--- leetCode/hash/word-pattern.py
+++ leetCode/hash/word-pattern.py
@@ -4,6 +4,7 @@
 
 # Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
 
+# sol 1:
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         s = s.split()
@@ -16,4 +17,23 @@ class Solution:
                 return False
             hashS[c] = s[i]
 
+        return True
+
+# sol 2:
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split(" ")
+        if len(pattern) != len(words):
+            return False
+        charToWord = {}
+        wordToChar = {}
+        
+        for c, w in zip(pattern, words):
+            if c in charToWord and charToWord[c] != w:
+                return False
+            if w in wordToChar and wordToChar[w] != c:
+                return False
+            charToWord[c] = w
+            wordToChar[w] = c
         return True
