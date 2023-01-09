263c7a0c86345f1576b0a84d3dfe592e1d7abc2f
ahmad1091 <ahmadtayeb91@gmail.com>
2023-01-09 20:32:13 +0200

    leetCode/Tree/binary-tree-postorder-traversal.py

---
+++ leetCode/Tree/binary-tree-postorder-traversal.py
@@ -0,0 +1,25 @@
+# 145. Binary Tree Postorder Traversal
+
+# Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def dfs(root):
+            if not root:
+                return
+
+            dfs(root.left)
+
+            dfs(root.right)
+
+            res.append(root.val)
+
+        dfs(root)
+        return res
