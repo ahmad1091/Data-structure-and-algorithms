f6b536eade34a9bfa0d50ed6b389c53de03e87b1
ahmad1091 <ahmadtayeb91@gmail.com>
2023-01-11 00:10:39 +0200

    leetCode/Tree/check-completeness-of-a-binary-tree.py

---
+++ leetCode/Tree/check-completeness-of-a-binary-tree.py
@@ -0,0 +1,23 @@
+# 958. Check Completeness of a Binary Tree
+
+# Given the root of a binary tree, determine if it is a complete binary tree.
+
+# In a complete binary tree, every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
+        q = deque([root])
+
+        while q:
+            node = q.popleft()
+            if not node:
+                break
+            q.append(node.left)
+            q.append(node.right)
+        return all( not node for node in q)
