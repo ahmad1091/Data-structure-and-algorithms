d12539e38ad76cce9326009c352123117d958666
ahmad1091 <ahmadtayeb91@gmail.com>
2023-01-10 21:37:07 +0200

    Cracking_the_Coding_Interview/chapter8/

--- leetCode/DynamicProgramming/coin-change-ii.py
+++ leetCode/DynamicProgramming/coin-change-ii.py
@@ -9,12 +9,16 @@
 # The answer is guaranteed to fit into a signed 32-bit integer.
 
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] * (amount + 1)
-        dp [0] = 0
-        for a in range(1, amount + 1):
-            for c in coins:
-                if a - c >= 0:
-                    dp[a] = min(dp[a], 1 + dp[a - c])
+    def change(self, amount: int, coins: List[int]) -> int:
+        cache = {}
+        def dfs(i, a):
+            if a == amount: return 1
+            if a > amount: return 0
+            if i == len(coins): return 0
+            if (i, a) in cache: return cache[(i, a)]
 
-        return dp[amount] if dp[amount] != amount + 1 else -1
+            cache[(i, a)] = dfs(i, a + coins[i]) + dfs(i + 1, a)
+            return cache[(i, a)]
+        
+        return dfs(0, 0)
+
