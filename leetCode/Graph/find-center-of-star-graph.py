db26756355f327ddd54a14d9d27490c5ef5901b0
ahmad1091 <ahmadtayeb91@gmail.com>
2023-01-15 14:26:58 +0200

    leetCode/Graph/find-center-of-star-graph.py sol2

--- leetCode/Graph/find-center-of-star-graph.py
+++ leetCode/Graph/find-center-of-star-graph.py
@@ -14,3 +14,8 @@ class Solution:
             common &= s
     
         return list(common)[0]
+
+# sol 2: more optimal 
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        return edges[0][0] if edges[0][0] in edges[1] else edges[0][1]
