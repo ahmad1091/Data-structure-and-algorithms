d7f1da4bb0ef48a606208bce293051044b72628d
ahmad1091 <ahmadtayeb91@gmail.com>
2023-01-17 15:11:13 +0200

    leetCode/Graph/all-paths-from-source-to-target.py

--- leetCode/Graph/all-paths-from-source-to-target.py
+++ leetCode/Graph/all-paths-from-source-to-target.py
@@ -4,6 +4,7 @@
 
 # The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
 
+# sol 1: bfs
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         q = deque([[0]])
@@ -19,4 +20,20 @@ class Solution:
             for m in graph[u]:
                 q.append(path + [m])
 
+        return res
+
+# sol 2: dfs
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        res = []
+        def dfs(p):
+            if p[-1] == len(graph) -1:
+                res.append(p[:])
+                return 
+            for v in graph[p[-1]]:
+                p.append(v)
+                dfs(p)
+                p.pop()
+        
+        dfs([0])
         return res
