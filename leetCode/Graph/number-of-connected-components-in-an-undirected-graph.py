4d19cf1e851c712aa8d95e4b9613bf0352ee93df
ahmad1091 <ahmadtayeb91@gmail.com>
2023-01-19 19:54:38 +0200

    leetCode/Graph/number-of-connected-components-in-an-undirected-graph.py

--- leetCode/Graph/number-of-connected-components-in-an-undirected-graph.py
+++ leetCode/Graph/number-of-connected-components-in-an-undirected-graph.py
@@ -32,4 +32,17 @@ class Solution:
         for n1, n2 in edges:
             res -= union(n1, n2)
         return res
+
+        # sol 2:
+        class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        def find(x):
+            if p[x] != x:
+                p[x] = find(p[x])
+            return p[x]
+
+        p = list(range(n))
+        for a, b in edges:
+            p[find(a)] = find(b)
+        return sum(i == find(i) for i in range(n))
     
