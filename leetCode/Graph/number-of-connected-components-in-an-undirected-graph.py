89d72d1701be4ed196e6665313002351c7d01293
ahmad1091 <ahmadtayeb91@gmail.com>
2023-01-19 19:11:29 +0200

    leetCode/Graph/number-of-connected-components-in-an-undirected-graph.py

---
+++ leetCode/Graph/number-of-connected-components-in-an-undirected-graph.py
@@ -0,0 +1,35 @@
+# 323. Number of Connected Components in an Undirected Graph
+
+# You have a graph of n nodes. You are given an integer n and an array edges where edges[i] = [ai, bi] indicates that there is an edge between ai and bi in the graph.
+
+# Return the number of connected components in the graph.
+
+class Solution:
+    def countComponents(self, n: int,edges: List[List[int]]) -> int:
+        parent = [i for i in range(n)]
+        rank = [1] * n
+
+        def find(n1):
+            res = n1
+            while res != parent[res]:
+                parent[res] = parent[parent[res]]
+                res = parent[res]
+            return res
+
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+            if p1 == p2:
+                return 0
+            if rank[p2] > rank[p1]:
+                parent[p1] = p2
+                rank[p2] += rank[p1]
+            else:
+                parent[p2] = p1
+                rank[p1] += rank[p2]
+            return 1
+
+        res = n
+        for n1, n2 in edges:
+            res -= union(n1, n2)
+        return res
+    
