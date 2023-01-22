e9bb5eba4b1408b68b757d63891e9e3813ec7163
ahmad1091 <ahmadtayeb91@gmail.com>
2023-01-22 14:49:02 +0200

    leetCode/TwoPointers/valid-palindrome-ii.py

--- leetCode/TwoPointers/valid-palindrome-ii.py
+++ leetCode/TwoPointers/valid-palindrome-ii.py
@@ -12,3 +12,20 @@ class Solution:
             l += 1
             r -= 1
         return True
+
+# sol 2:
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def check(i, j):
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i, j = i + 1, j - 1
+            return True
+
+        i, j = 0, len(s) - 1
+        while i < j:
+            if s[i] != s[j]:
+                return check(i, j - 1) or check(i + 1, j)
+            i, j = i + 1, j - 1
+        return True
