2ce71cc0fe2f09ee9495ddfba00a8f273a9268fe
ahmad1091 <ahmadtayeb91@gmail.com>
2023-01-22 14:25:05 +0200

    leetCode/TwoPointers/reverse-words-in-a-string.py

--- leetCode/TwoPointers/reverse-words-in-a-string.py
+++ leetCode/TwoPointers/reverse-words-in-a-string.py
@@ -19,3 +19,32 @@ class Solution:
             else: break
 
         return ' '.join(strArr)
+
+# sol 2 : reverse all the string before
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = re.sub(' +',' ',s.strip())
+        s = list(s)
+        length = len(s)
+        self.reverser(s, 0, length - 1)
+        start = end = 0
+
+        while True:
+            while start < length and s[start] == ' ':
+                start += 1
+
+            if start == length :break
+
+            end = start + 1
+            while end < length and s[end] != ' ':
+                end += 1
+            
+            self.reverser(s, start, end - 1)
+            start = end
+        return ''.join(s)
+
+    def reverser(self, string, start, end):
+        while start < end:
+            string[start], string[end] = string[end], string[start]
+            start += 1
+            end -= 1
