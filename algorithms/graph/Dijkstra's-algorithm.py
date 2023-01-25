6cb481866ca93387b3762dcb2ee4416695674984
ahmad1091 <ahmadtayeb91@gmail.com>
2023-01-25 14:22:38 +0200

    algorithms/graph/Dijkstra's-algorithm.py

--- algorithms/graph/Dijkstra's-algorithm.py
+++ algorithms/graph/Dijkstra's-algorithm.py
@@ -1,3 +1,15 @@
+# Follow the steps below to solve the problem:
+
+# Create a set sptSet (shortest path tree set) that keeps track of vertices included in the shortest-path tree, i.e., whose minimum distance from the source is calculated and finalized. Initially, this set is empty. 
+# Assign a distance value to all vertices in the input graph. Initialize all distance values as INFINITE. Assign the distance value as 0 for the source vertex so that it is picked first. 
+# While sptSet doesn’t include all vertices 
+# Pick a vertex u which is not there in sptSet and has a minimum distance value. 
+# Include u to sptSet. 
+# Then update distance value of all adjacent vertices of u. 
+# To update the distance values, iterate through all adjacent vertices. 
+# For every adjacent vertex v, if the sum of the distance value of u (from source) and weight of edge u-v, is less than the distance value of v, then update the distance value of v. 
+# Note: We use a boolean array sptSet[] to represent the set of vertices included in SPT. If a value sptSet[v] is true, then vertex v is included in SPT, otherwise not. Array dist[] is used to store the shortest distance values of all vertices.
+
 # Python program for Dijkstra's single
 # source shortest path algorithm. The program is
 # for adjacency matrix representation of the graph
