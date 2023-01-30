3df9d0f0dd1e94469e8426462c421381a4394458
ahmad1091 <ahmadtayeb91@gmail.com>
2023-01-30 15:57:49 +0200

    leetCode/palindrome-number.py

--- leetCode/palindrome-number.py
+++ leetCode/palindrome-number.py
@@ -6,5 +6,16 @@
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-
         return str(x) == str(x)[::-1]
+
+# sol 2: Math
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0: return False
+        y, t = 0, x
+        while t:
+            y = y * 10 + t % 10
+            print(y)
+            t //= 10
+        
+        return x == y
