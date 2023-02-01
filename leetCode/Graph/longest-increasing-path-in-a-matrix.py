e152f1528271030e2ee84298e3ab0110c023e76f
ahmad1091 <ahmadtayeb91@gmail.com>
2023-02-01 13:24:08 +0200

    leetCode/Graph/longest-increasing-path-in-a-matrix.py

--- leetCode/Graph/longest-increasing-path-in-a-matrix.py
+++ leetCode/Graph/longest-increasing-path-in-a-matrix.py
@@ -29,3 +29,18 @@ class Solution:
                 dfs(r, c, -1)
 
         return max(dp.values())
+
+# sol 2:
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        @cache
+        def dfs(i, j):
+            ans = 1
+            for a, b in pairwise((1, 0, -1, 0, 1)):
+                x, y = i + a, j + b
+                if 0 <= x < m and 0 <= y < n and matrix[x][y] > matrix[i][j]:
+                    ans = max(ans, dfs(x, y) + 1)
+            return ans
+
+        m, n = len(matrix), len(matrix[0])
+        return max(dfs(i, j) for i in range(m) for j in range(n))
