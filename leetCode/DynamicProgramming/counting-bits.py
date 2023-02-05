4cd375a2d1fbb2adb66a7b7096e2ca7e8e114627
ahmad1091 <ahmadtayeb91@gmail.com>
2023-02-05 15:14:13 +0200

    leetCode/DynamicProgramming/counting-bits.py

--- leetCode/DynamicProgramming/counting-bits.py
+++ leetCode/DynamicProgramming/counting-bits.py
@@ -20,4 +20,16 @@ class Solution:
                 count += (b % 2)
                 b //= 2
             res[i] = count
-        return res 
+        return res
+
+# sol 3: DP
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        offset = 1
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+            
+        return dp 
