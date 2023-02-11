dd49de1075400060d7a458d85a095f548c1a84fb
ahmad1091 <ahmadtayeb91@gmail.com>
2023-02-11 15:07:19 +0200

    leetCode/DynamicProgramming/minimum-path-sum.py

--- leetCode/DynamicProgramming/minimum-path-sum.py
+++ leetCode/DynamicProgramming/minimum-path-sum.py
@@ -4,19 +4,42 @@
 
 # Note: You can only move either down or right at any point in time.
 
-sol 1:
+# sol 1:
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
         maxInt =  2**31 - 1
-
+        cache = {}
  
         def dfs(m, n):
+            if (m, n) in cost: return cache[(m, n)]
             if (n < 0 or m < 0):
                 return maxInt
             elif (m == 0 and n == 0):
                 return grid[m][n]
             else:
-                return grid[m][n] + min(dfs(m-1, n),dfs(m, n-1))
+                cost = grid[m][n] + min(dfs(m-1, n),dfs(m, n-1))
+                cache[(m, n)] = cost
+                return cost
+
+        return dfs(ROWS - 1, COLS - 1)
+
+# sol 2: dp solution
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        maxInt =  2**31 - 1
+        cache = {}
+ 
+        def dfs(m, n):
+            if (m, n) in cache: return cache[(m, n)]
+            if (n < 0 or m < 0):
+                return maxInt
+            elif (m == 0 and n == 0):
+                return grid[m][n]
+            else:
+                cost = grid[m][n] + min(dfs(m-1, n),dfs(m, n-1))
+                cache[(m, n)] = cost
+                return cost
 
         return dfs(ROWS - 1, COLS - 1)
