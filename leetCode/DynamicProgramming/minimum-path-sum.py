42a79288297e079167a85a2b64dcd5262176a5f5
ahmad1091 <ahmadtayeb91@gmail.com>
2023-02-11 17:18:51 +0200

    leetCode/DynamicProgramming/minimum-path-sum.py

--- leetCode/DynamicProgramming/minimum-path-sum.py
+++ leetCode/DynamicProgramming/minimum-path-sum.py
@@ -24,7 +24,7 @@ class Solution:
 
         return dfs(ROWS - 1, COLS - 1)
 
-# sol 2: dp solution
+# sol 2: dp solution O(m * n)
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
@@ -43,3 +43,15 @@ class Solution:
                 return cost
 
         return dfs(ROWS - 1, COLS - 1)
+
+# sol 3:
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        dp = [[float('inf')] * (COLS + 1) for _ in range(ROWS + 1)]
+        dp[ROWS - 1][COLS] = 0
+
+        for r in range(ROWS - 1, -1, -1):
+            for c in range(COLS - 1, -1, -1):
+                dp[r][c] = grid[r][c] + min(dp[r + 1][c], dp[r][c + 1])
+        return dp[0][0]
