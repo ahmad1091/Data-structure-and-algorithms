94ad789bab635dc6b045dce41cab22846b58d7f8
ahmad1091 <ahmadtayeb91@gmail.com>
2023-02-15 15:33:10 +0200

    leetCode/DynamicProgramming/student-attendance-record-ii.py

--- leetCode/DynamicProgramming/student-attendance-record-ii.py
+++ leetCode/DynamicProgramming/student-attendance-record-ii.py
@@ -29,3 +29,26 @@ class Solution:
 
             return temp
         return dfs(n, 0, False)
+
+# sol 2: DP
+class Solution:
+    def checkRecord(self, n: int) -> int:
+        mod = int(1e9 + 7)
+        dp = [[[0, 0], [0, 0], [0, 0]] for _ in range(n)]
+
+        dp[0][0][0] = dp[0][0][1] = dp[0][1][0] = 1
+        
+        for i in range(1, n):
+            dp[i][0][0] = (dp[i - 1][0][0] + dp[i - 1][1][0] + dp[i - 1][2][0]) % mod
+            dp[i][0][1] = (dp[i - 1][0][0] + dp[i - 1][1][0] + dp[i - 1][2][0]) % mod + (dp[i - 1][0][1] + dp[i - 1][1][1] + dp[i - 1][2][1]) % mod
+            dp[i][1][0] = dp[i - 1][0][0] % mod
+            dp[i][1][1] = dp[i - 1][0][1] % mod
+            dp[i][2][0] = dp[i - 1][1][0] % mod
+            dp[i][2][1] = dp[i - 1][1][1] % mod
+        res = 0
+        for i in range(3):
+            for j in range(2):
+               res += dp[-1][i][j]
+               res %= mod
+        return res
+            
