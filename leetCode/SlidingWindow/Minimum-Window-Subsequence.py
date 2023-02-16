8921022d99ca388e3cbafe6e94c8d4a102664d7b
ahmad1091 <ahmadtayeb91@gmail.com>
2023-02-16 13:08:40 +0200

    leetCode/SlidingWindow/Minimum-Window-Subsequence.py

---
+++ leetCode/SlidingWindow/Minimum-Window-Subsequence.py
@@ -0,0 +1,6 @@
+# 727. Minimum Window Subsequence
+
+# Given strings s1 and s2, return the minimum contiguous substring part of s1, so that s2 is a subsequence of the part.
+
+# If there is no such window in s1 that covers all characters in s2, return the empty string "". If there are multiple such minimum-length windows, return the one with the left-most starting index.
+
