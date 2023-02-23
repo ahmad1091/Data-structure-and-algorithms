e64bb73232f3e05673dacd4d8285bea264f4626b
ahmad1091 <ahmadtayeb91@gmail.com>
2023-02-23 15:28:01 +0200

    leetCode/DynamicProgramming/longest-palindromic-substring.py

--- leetCode/DynamicProgramming/longest-palindromic-substring.py
+++ leetCode/DynamicProgramming/longest-palindromic-substring.py
@@ -17,3 +17,23 @@ class Solution:
                     res = s[l:r + 1]
                 r += 1
         return res
+
+# sol 2/:
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        def palCheck(l, r):
+            while l >= 0 and r < n and s[l] == s[r]:
+                l, r = l - 1, r + 1
+            return r - l - 1 
+        res = ''
+        for i in range(n):
+            odd = palCheck(i,i)
+            even = palCheck(i, i + 1)
+            t = max(odd, even)
+            if t > len(res):
+                rightBound = i - ((t - 1) >> 1)
+                leftBound = rightBound + t
+                res = s[rightBound: leftBound]
+
+        return res
