54987e3d96cff7d9035cd9187b0d9a030e57ae20
ahmad1091 <ahmadtayeb91@gmail.com>
2023-02-27 12:31:42 +0200

    leetCode/SlidingWindow/repeated-dna-sequences.py

--- leetCode/SlidingWindow/repeated-dna-sequences.py
+++ leetCode/SlidingWindow/repeated-dna-sequences.py
@@ -7,6 +7,7 @@
 
 # Given a string s that represents a DNA sequence, return all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule. You may return the answer in any order.
 
+# sol 1: Brut force
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         res = []
@@ -15,3 +16,16 @@ class Solution:
             if s[i:i + 10] in s[i + 1:]:
                 res.append(s[i:i + 10])
         return list(set(res))
+
+# sol 2:
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        res, seen = [], set()
+
+        for l in range(len(s) - 9):
+            cur = s[l:l + 10]
+            if (cur in seen) and (cur not in res):
+                res.append(cur)
+            seen.add(cur)
+        return res
+            
