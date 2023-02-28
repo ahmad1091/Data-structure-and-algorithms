0dc47038360df0e237953b9c9c8a05d9b6d5ab65
ahmad1091 <ahmadtayeb91@gmail.com>
2023-02-28 15:10:22 +0200

    leetCode/Sorting/insert-interval.py

--- leetCode/Sorting/insert-interval.py
+++ leetCode/Sorting/insert-interval.py
@@ -20,3 +20,28 @@ class Solution:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
         res.append(newInterval)
         return res
+
+# sol 2:
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        new_start, new_end = newInterval[0], newInterval[1]
+        i, n = 0, len(intervals)
+
+        while i < n and intervals[i][0] < new_start:
+            res.append(intervals[i])
+            i += 1
+        if not res or new_start > res[-1][1]:
+            res.append(newInterval)
+        else:
+            res[-1][1] = max(new_end, res[-1][1])
+
+        while i < n:
+            start, end = intervals[i]
+            if start > res[-1][1]:
+                res.append(intervals[i])
+            else:
+                res[-1][1] = max(end, res[-1][1])
+            i += 1
+            
+        return res
