66f634aac9965e8cb3b0259928bbc8758053ac48
ahmad1091 <ahmadtayeb91@gmail.com>
2023-03-01 14:21:17 +0200

    leetCode/Sorting/meeting-rooms.py

---
+++ leetCode/Sorting/meeting-rooms.py
@@ -0,0 +1,10 @@
+# Description
+# Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals.sort(key=lambda x: x[0])
+        for i in range(len(intervals) - 1):
+            if intervals[i][1] > intervals[i + 1][0]:
+                return False
+        return True
