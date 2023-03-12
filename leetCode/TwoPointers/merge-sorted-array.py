c2fed63dc7c3c32f8f5d8205f2634b2aefac9b9b
ahmad1091 <ahmadtayeb91@gmail.com>
2023-03-12 21:12:59 +0200

    leetCode/TwoPointers/merge-sorted-array.py

--- leetCode/TwoPointers/merge-sorted-array.py
+++ leetCode/TwoPointers/merge-sorted-array.py
@@ -44,3 +44,15 @@ class Solution:
             else:
                 nums1[p] = nums2[j]
                 j -= 1
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        for p in range(n + m - 1, -1, -1):
+            if n <= 0:
+                break
+            if m > 0 and nums1[m - 1] > nums2[n - 1]:
+                nums1[p] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[p] = nums2[n - 1]
+                n -= 1
