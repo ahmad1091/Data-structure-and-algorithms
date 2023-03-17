89366b6cc262b3aafe5507253f23987d4defd38b
ahmad1091 <ahmadtayeb91@gmail.com>
2023-03-17 17:50:48 +0200

    leetCode/hash/top-k-frequent-elements.ipynb

--- leetCode/Heap(PriorityQueue)/top-k-frequent-elements.py
+++ leetCode/Heap(PriorityQueue)/top-k-frequent-elements.py
@@ -1,6 +1,6 @@
-347. Top K Frequent Elements
+# 347. Top K Frequent Elements
 
-Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+# Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 
 # solution 1: using Hash
 class Solution:
