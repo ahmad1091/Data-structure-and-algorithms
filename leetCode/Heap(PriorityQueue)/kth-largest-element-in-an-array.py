2e3d448719e57fd66ee87dd7bd2a32ca2eb6932a
ahmad1091 <ahmadtayeb91@gmail.com>
2023-03-18 11:23:00 +0200

    leetCode/Heap(PriorityQueue)/kth-largest-element-in-an-array.py

--- leetCode/Heap(PriorityQueue)/kth-largest-element-in-an-array.py
+++ leetCode/Heap(PriorityQueue)/kth-largest-element-in-an-array.py
@@ -47,3 +47,19 @@ class Solution:
                 heapq.heappop(pq)
 
         return pq[0]
+
+# sol 4:
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        min_heap = []
+
+        for i in range(k):
+            heappush(min_heap, nums[i])
+
+        for i in  range(k, len(nums)):
+            if nums[i] > min_heap[0]:
+                heappop(min_heap)
+                heappush(min_heap, nums[i])
+
+
+        return  min_heap[0]
