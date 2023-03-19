92e5d3cd8f0d825112c0fc0f3072ac87303e8c74
ahmad1091 <ahmadtayeb91@gmail.com>
2023-03-19 14:58:42 +0200

    leetCode/BinarySearch/search-in-rotated-sorted-array.py

--- leetCode/BinarySearch/search-in-rotated-sorted-array.py
+++ leetCode/BinarySearch/search-in-rotated-sorted-array.py
@@ -45,4 +45,27 @@ class Solution:
                 else:
                     l = mid + 1
 
-        return -1     
+        return -1 
+
+# sol 3:
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        return self.binary_search(nums, 0, len(nums) - 1, target)
+
+    def binary_search(self, nums: List[int], low: int, high:int, target: int):
+        if low > high: return -1
+
+        mid = (high + low) >> 1
+
+        if nums[mid] == target: return mid
+
+        if nums[low] <= nums[mid]:
+            if nums[low] <= target < nums[mid]:
+                return self.binary_search(nums, low, mid - 1, target)
+            else:
+                return self.binary_search(nums, mid + 1, high, target)
+        else:
+            if nums[mid] < target <= nums[high]:
+                return self.binary_search(nums, mid + 1, high, target)
+            else:
+                return self.binary_search(nums, low, mid - 1, target)
